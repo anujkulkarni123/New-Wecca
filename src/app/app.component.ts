4bb8cd8ee7bc78ed02ebc75100d8d619918b8615
swan484 <swan484@uwo.ca>
2021-01-13 22:21:02 -0500

    Bootstrap and scrolling animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { AotSummaryResolver } from '@angular/compiler';
+import { Component, ModuleWithComponentFactories, OnInit } from '@angular/core';
+
+import * as AOS from 'aos';
+declare var $:any;
+import 'ekko-lightbox';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'weccaWebsite';
+
+  ngOnInit(){
+    AOS.init();
+    
+    $(document).on('click', '[data-toggle="lightbox"]', function(event) {
+      event.preventDefault();
+      $(this).ekkoLightbox();
+  });
+  }
+
 }
+
