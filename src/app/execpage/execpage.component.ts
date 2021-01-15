2f79fe0eb572d6bba60b6c1d71286fccaebeab9e
Ethan Bodnar <ethanbodnar@ethans-mbp.lan>
2021-01-15 10:46:09 -0700

    Touched up execpage and description functionality

--- src/app/execpage/execpage.component.ts
+++ src/app/execpage/execpage.component.ts
@@ -17,10 +17,35 @@ export class ExecpageComponent implements OnInit {
 
   ngOnInit() {
   }
-
+  //I will look into dictionaries, for now I just wanted to get this function working
+  /*
+  Function to change the text under the executive description as well as the height of the gallery box on click
+  */
   changeText(event, name, id){
     var text;
     var info = document.getElementById(id).innerHTML;
+    var tag = "";
+    var ind = 0;
+  
+    if(name == 'will' || name == 'megan' || name == 'sneha'){
+      tag = "gallery-1";
+    }
+    else if(name == "anuj" || name == "ethan" || name == "andrew"){
+      tag = "gallery-2";
+    }
+    else if(name == "anthony" || name == "lauren" || name == "keston"){
+      tag = "gallery-3";
+    }
+    else if(name == "liam" || name == "jen" || name == "trevor"){
+      tag = "gallery-4";
+    }
+    else if(name == "jericho" || name == "kara" || name == "sierra"){
+      tag = "gallery-5";
+    }
+    else if(name == "sam" || name == "mac"){
+      tag = "gallery-6";
+    }
+    
     if(name == 'will' && info == ''){
       text = "Insert Will's Description";
     }
@@ -34,12 +59,12 @@ export class ExecpageComponent implements OnInit {
       text = "Insert Anuj's Description";
     }
     else if(name == 'ethan' && info == ''){
-      text = "Insert Ethan's Description";
+      text = "Insert Ethan's Description; here is what a multi line description would look like here";
     }
     else if(name == 'andrew' && info == ''){
       text = "Indert Andrew's Description";
     }
-    else if(name == 'tony' && info == ''){
+    else if(name == 'anthony' && info == ''){
       text = "Insert Anthony's Description";
     }
     else if(name == 'lauren' && info == ''){
@@ -55,7 +80,7 @@ export class ExecpageComponent implements OnInit {
       text = "Insert Jen's Description";
     }
     else if(name == 'trevor' && info == ''){
-      text = "Indert Trevor's Description";
+      text = "Insert Trevor's Description";
     }
     else if(name == 'jericho' && info == ''){
       text = "Insert Jericho's Description";
@@ -75,7 +100,42 @@ export class ExecpageComponent implements OnInit {
     else {
       text ='';
     }
-    document.getElementById(id).innerHTML = text;
-  }
 
+    document.getElementById(id).innerHTML = text;
+    if(text != ''){
+      document.getElementById(tag).style.gridTemplateRows = "55vmin";
+    }
+    else{
+      var rowNum = Number(tag.slice(8,9));
+      var elemMin = 1;
+
+      if(rowNum > 1){
+        elemMin = ((rowNum - 1)* 3);
+      }
+      else{
+        elemMin = 0;
+      }
+
+      if(rowNum < 6){
+        var inc = 3;
+      }
+      else{
+        inc = 2;
+      }
+
+      var IDs = document.getElementsByClassName('subtext');
+      for(var i = elemMin; i < elemMin + inc; i++){
+        if(IDs[i].innerHTML != ''){
+          ind ++;
+        }
+      }
+
+      if(ind == 0){
+        document.getElementById(tag).style.gridTemplateRows = "45vmin";
+      }
+      else {
+        document.getElementById(tag).style.gridTemplateRows = "55vmin";
+      }
+    }
+  }
 }
