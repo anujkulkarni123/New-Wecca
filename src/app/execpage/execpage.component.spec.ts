7395d490cfbe8f0bf4ecbbd79fff0cdf604e203f
ebodnar1 <ebodnar@uwo.ca>
2021-09-13 21:50:08 -0400

    Added new exec photos and names. A few other little style changes

--- src/app/execpage/execpage.component.spec.ts
+++ src/app/execpage/execpage.component.spec.ts
@@ -24,13 +24,4 @@ describe('ExecpageComponent', () => {
   });
 });
 
-function swapText(){
-  var info = document.getElementById("will-info").innerHTML;
-  var name = document.getElementById("will-name").innerHTML;
-  if(name === "Will Stevenson"){
-    document.getElementById("will-info").innerHTML = "Description";
-  }
-  else{
-    document.getElementById("will-info").innerHTML = "Will Stevenson"
-  }
-}
+
