44af20fc03d17f21e4162842a53535d49bd3e0ef
Tristan Jin <tjin368@gmail.com>
2022-01-22 15:30:47 -0500

    Made skeleton for teams page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { HomepageComponent } from './homepage/homepage.component';
 import { CalendarComponent } from './calendar/calendar.component';
 import { ExecpageComponent } from './execpage/execpage.component';
 import { SponsorsComponent } from './sponsors/sponsors.component';
-import { newPageComponent } from './newPage/newPage.component';
+import { teamsComponent } from './teams/teams.component';
 
 
 
@@ -14,7 +14,7 @@ const routes: Routes = [
   {path:"calendar", component: CalendarComponent},
   {path:"executives", component: ExecpageComponent},
   {path:"sponsorship", component: SponsorsComponent},
-  {path:"newPage", component: newPageComponent}
+  {path:"teams", component: teamsComponent}
 ];
 
 @NgModule({
